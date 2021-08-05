bc94a5d2e0b4c390f932b8adcd5f0223b0616cef
Aya <aya.moustafa.elgedy@hotmail.com>
2021-08-05 15:07:47 +0200

    fix role

--- 11.assign_role.tf
+++ 11.assign_role.tf
@@ -1,13 +1,20 @@
 data "google_iam_policy" "admin" {
   binding {
-    role = "roles/iam.serviceAccountUser"
-    # compute.instanceAdmin
-
-    members = ["allAuthenticatedUsers"]
+    role = "roles/compute.instanceAdmin"
+    members = [
+      "user:aya.moustafa.fahmy@gmail.com",
+    ]  
   }
 }
 
-resource "google_service_account_iam_policy" "admin-account-iam" {
-  service_account_id = google_service_account.service_account.name
-  policy_data        = data.google_iam_policy.admin.policy_data
+resource "google_compute_instance_iam_policy" "policy" {
+  project = google_compute_instance.public_vm.project
+  zone = google_compute_instance.public_vm.zone
+  instance_name = google_compute_instance.public_vm.name
+  policy_data = data.google_iam_policy.admin.policy_data
 }
+
+# resource "google_service_account_iam_policy" "admin-account-iam" {
+#   service_account_id = google_service_account.service_account.name
+#   policy_data        = data.google_iam_policy.admin.policy_data
+# }
